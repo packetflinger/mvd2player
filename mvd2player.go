3b723d58204bccf5c08079228ec8b90c349dd936
Joe Reid <joe@joereid.com>
2022-03-28 15:22:20 -0400

    Helpful error messages

--- mvd2player.go
+++ mvd2player.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"os/user"
@@ -21,17 +23,34 @@ func main() {
 	configfile := fmt.Sprintf("%s%smvd2player.json", user.HomeDir, sep)
 
 	configbody, err := os.ReadFile(configfile)
-	iferr(err)
+	if err != nil {
+		fmt.Printf("Problems loading config file: %s\n", configfile)
+		fmt.Println("Please make sure you create that file and add your config like:")
+		fmt.Printf("{\n  \"baseq2folder\": \"c:/q2/baseq2\",\n")
+		fmt.Printf("  \"q2binary\": \"c:/q2/q2pro.exe\"\n}\n")
+		fmt.Printf("\n\nPress ENTER key to quit\n")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
+		return
+	}
 
 	var config Config
 	err = json.Unmarshal(configbody, &config)
 	iferr(err)
 
+	// wasn't given demo file as argument
+	if len(os.Args) < 2 {
+		fmt.Printf("No .mvd2 file supplied as argument. Set this program as the default application for .mvd2 files.\n")
+		fmt.Printf("\n\nPress ENTER key to quit\n")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
+		return
+	}
+
 	// copy the demo the right place
 	demoname := fmt.Sprintf("%s%s%s%stempdemo.mvd2", config.Baseq2, sep, "demos", sep)
+
+	// "copy" demo to a temp file in the right location
 	demosrc, err := os.ReadFile(os.Args[1])
 	iferr(err)
-
 	err = os.WriteFile(demoname, demosrc, 0666)
 	iferr(err)
 
@@ -41,11 +60,11 @@ func main() {
 	err = os.WriteFile(cfgname, []byte(cfg), 0666)
 	iferr(err)
 
-	// spawn a quake 2 process to start playing the demo
+	// spawn a q2pro process to start playing the demo, block until completed
 	cmd := exec.Command(config.Q2exe, "+exec", "tempmvd.cfg")
 	_ = cmd.Run()
 
-	// remove
+	// remove temp demo and config
 	err = os.Remove(demoname)
 	iferr(err)
 
@@ -55,6 +74,9 @@ func main() {
 
 func iferr(e error) {
 	if e != nil {
-		panic(e)
+		log.Println(e)
+		fmt.Printf("\n\nPress ENTER key to quit\n")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
+		return
 	}
 }
